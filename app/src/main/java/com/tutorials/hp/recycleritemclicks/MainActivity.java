35c26fd0051af2c2c8d698e308fc804c10974c4d
Oclemy <oclemmi@gmail.com>
2017-08-28 08:37:46 +0300

    Updated MainActivity

--- app/src/main/java/com/tutorials/hp/recycleritemclicks/MainActivity.java
+++ app/src/main/java/com/tutorials/hp/recycleritemclicks/MainActivity.java
@@ -6,18 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-
 import com.tutorials.hp.recycleritemclicks.mRecycler.MyAdapter;
-
+/*
+- Our MainActivity class.
+- Derives from AppCompatActivity which resides in the support library.
+- Methods: onCreate().
+- Inflated From content_main.xml using the setContentView() method.
+- The views we use are RecyclerView.
+- Reference RecyclerView from our layout specification using findViewById().
+- Instantiate adapter and set it to RecyclerView.
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /*
+    DECALARATIONS
+     */
     RecyclerView rv;
     MyAdapter adapter;
     String[] spacecrafts={
             "Casini","Challenger","Spirit","Opportunity","Curiosity","Enterprise","Pioneer","Spitzer","Voyager",
             "Rosetter","Columbia","Apollo 15","Apollo 17","Kepler"
     };
-
+    /*
+    WHEN ACTIVITY IS CREATED.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         //RFERENCE VIEWS
         rv = (RecyclerView) findViewById(R.id.rv);
-
         //RECYCLER PROPERTIES
         rv.setLayoutManager(new LinearLayoutManager(this));
-
         //ADAPTER
         adapter = new MyAdapter(this, spacecrafts);
         rv.setAdapter(adapter);
